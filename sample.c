2e14c5e6c0438dc974a093142080571b8f3c56df
mauricethegoat <mauricewisecup@gmail.com>
2023-05-03 06:19:40 +0000

    project

--- sample.c
+++ sample.c
@@ -1,7 +1,76 @@
-#include <stdio.h>
-int main() {
-      printf("hello world");
-	printf("from branch staging");
-	printf("add new features");	
-	
-}
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Academic Calendar 2022-2023</title>
+    <style>
+        table {
+		      border-collapse: collapse;
+		            width: 100%;
+				       }
+    th, td {
+	          text-align: left;
+		        padding: 8px;
+				     }
+    th {
+	          background-color: #f2f2f2;
+		      }
+    tr:nth-child(even) {
+	             background-color: #f2f2f2;
+		         }
+  </style>
+  </head>
+  <body>
+    <h1>Academic Calendar 2022-2023</h1>
+      <table>
+          <tr>
+	        <th>Date</th>
+		      <th>Event</th>
+		          </tr>
+			      <tr>
+			            <td>August 30, 2022</td>
+				          <td>First Day of Fall Semester</td>
+					      </tr>
+					          <tr>
+						        <td>September 3, 2022</td>
+							      <td>Last Day to Add/Drop Courses</td>
+							          </tr>
+								      <tr>
+								            <td>October 7-11, 2022</td>
+									          <td>Midterm Exams</td>
+										      </tr>
+										          <tr>
+											        <td>November 23-26, 2022</td>
+												      <td>Thanksgiving Break</td>
+												          </tr>
+													      <tr>
+													            <td>December 12-16, 2022</td>
+														          <td>Final Exams</td>
+															      </tr>
+															          <tr>
+																        <td>December 23, 2022 - January 1, 2023</td>
+																	      <td>Winter Break</td>
+																	          </tr>
+																		      <tr>
+																		            <td>January 8, 2023</td>
+																			          <td>First Day of Spring Semester</td>
+																				      </tr>
+																				          <tr>
+																					        <td>January 15, 2023</td>
+																						      <td>Last Day to Add/Drop Courses</td>
+																						          </tr>
+																							      <tr>
+																							            <td>March 13-17, 2023</td>
+																								          <td>Spring Break</td>
+																									      </tr>
+																									          <tr>
+																										        <td>April 28 - May 2, 2023</td>
+																											      <td>Final Exams</td>
+																											          </tr>
+																												      <tr>
+																												            <td>May 12, 2023</td>
+																													          <td>Last Day of Spring Semester</td>
+																														      </tr>
+																														        </table>
+																															</body>
+																															</html>
+
